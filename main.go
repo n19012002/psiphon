d39a7aa740d11b4a097a07ab51b06f8558861b30
n19012002 <118285233+n19012002@users.noreply.github.com>
2024-10-22 08:24:17 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,170 +1,248 @@
-package main
+package libpsiphon
 
 import (
-	"flag"
+	"bufio"
+	"encoding/json"
 	"fmt"
 	"os"
-	"runtime"
+	"os/exec"
+	"strconv"
 	"strings"
 	"time"
 
-	"github.com/aztecrabbit/brainfuck-psiphon-pro-go/src/libpsiphon"
-	"github.com/aztecrabbit/libinject"
 	"github.com/aztecrabbit/liblog"
 	"github.com/aztecrabbit/libproxyrotator"
-	"github.com/aztecrabbit/libredsocks"
 	"github.com/aztecrabbit/libutils"
 )
 
-const (
-	appName        = "Brainfuck Tunnel"
-	appVersionName = "Psiphon Pro Go"
-	appVersionCode = "1.3.210109"
-
-	copyrightYear   = "2020"
-	copyrightAuthor = "Aztec Rabbit"
-)
-
 var (
-	InterruptHandler = new(libutils.InterruptHandler)
-	Redsocks         = new(libredsocks.Redsocks)
+	Loop          = true
+	DefaultConfig = &Config{
+		CoreName: "psiphon-tunnel-core",
+		Tunnel:   10,
+		Region:   "",
+		Protocols: []string{
+			"FRONTED-MEEK-HTTP-OSSH",
+			"FRONTED-MEEK-OSSH",
+		},
+		TunnelWorkers:  200,
+		KuotaDataLimit: 0,
+		Authorizations: make([]string, 0),
+	}
+	DefaultKuotaData = &KuotaData{
+		Port: make(map[int]map[string]float64),
+		All:  0,
+	}
+	ConfigPathPsiphon = libutils.GetConfigPath("brainfuck-psiphon-pro-go", "storage/psiphon")
 )
+
+func Stop() {
+	Loop = false
+}
+
+func RemoveData() {
+	os.RemoveAll(ConfigPathPsiphon + "/data")
+}
 
 type Config struct {
-	ProxyRotator *libproxyrotator.Config
-	Inject       *libinject.Config
-	PsiphonCore  int
-	Psiphon      *libpsiphon.Config
+	CoreName       string
+	Tunnel         int
+	Region         string
+	Protocols      []string
+	TunnelWorkers  int
+	KuotaDataLimit int
+	Authorizations []string
 }
 
-func init() {
-	InterruptHandler.Handle = func() {
-		libpsiphon.Stop()
-		libredsocks.Stop(Redsocks)
-		liblog.LogKeyboardInterrupt()
-	}
-	InterruptHandler.Start()
+type KuotaData struct {
+	Port map[int]map[string]float64
+	All  float64
 }
 
-func GetConfigPath(filename string) string {
-	return libutils.GetConfigPath("brainfuck-psiphon-pro-go", filename)
+type Data struct {
+	MigrateDataStoreDirectory string
+	UpstreamProxyURL          string
+	LocalSocksProxyPort       int
+	SponsorId                 string
+	PropagationChannelId      string
+	EmitBytesTransferred      bool
+	EmitDiagnosticNotices     bool
+	DisableLocalHTTPProxy     bool
+	EgressRegion              string
+	TunnelPoolSize            int
+	ConnectionWorkerPoolSize  int
+	LimitTunnelProtocols      []string
+	Authorizations            []string
 }
 
-func main() {
-	liblog.Header(
-		[]string{
-			fmt.Sprintf("%s [%s Version. %s]", appName, appVersionName, appVersionCode),
-			fmt.Sprintf("(c) %s %s.", copyrightYear, copyrightAuthor),
-		},
-		liblog.Colors["G1"],
-	)
+type Psiphon struct {
+	ProxyRotator    *libproxyrotator.ProxyRotator
+	Config          *Config
+	ProxyPort       string
+	KuotaData       *KuotaData
+	ListenPort      int
+	TunnelConnected int
+	Verbose         bool
+}
 
-	config := new(Config)
-	defaultConfig := new(Config)
-	defaultConfig.ProxyRotator = libproxyrotator.DefaultConfig
-	defaultConfig.Inject = libinject.DefaultConfig
-	defaultConfig.Inject.Type = 2
-	defaultConfig.Inject.Rules = map[string][]string{
-		"akamai.net:80": []string{
-			"125.235.36.177",
-		},
+func (p *Psiphon) LogInfo(message string, color string) {
+	if Loop {
+		liblog.LogInfo(message, strconv.Itoa(p.ListenPort), color)
 	}
-	defaultConfig.Inject.Payload = ""
-	defaultConfig.Inject.Timeout = 5
-	defaultConfig.PsiphonCore = 4
-	defaultConfig.Psiphon = libpsiphon.DefaultConfig
+}
 
-	if runtime.GOOS == "windows" {
-		defaultConfig.Psiphon.CoreName += ".exe"
+func (p *Psiphon) LogVerbose(message string, color string) {
+	if p.Verbose {
+		p.LogInfo(fmt.Sprintf("%[1]sVERBOSE%[3]s %[2]s::%[3]s %[1]s", color, liblog.Colors["P1"], liblog.Colors["CC"])+message, color)
 	}
+}
 
-	libutils.JsonReadWrite(GetConfigPath("config.json"), config, defaultConfig)
+func (p *Psiphon) GetAuthorizations() []string {
+	data := make([]string, 0)
 
-	var flagPro = true
-	var flagRefresh = false
-	var flagVerbose = false
-	var flagFrontend string
-	var flagWhitelist string
-
-	flag.BoolVar(&flagPro, "pro", flagPro, "Pro Version?")
-	flag.BoolVar(&flagRefresh, "refresh", flagRefresh, "Refresh Data")
-	flag.BoolVar(&flagVerbose, "verbose", flagVerbose, "Verbose Log?")
-	flag.StringVar(&flagFrontend, "f", flagFrontend, "-f frontend-domains (e.g. -f cdn.com,cdn.com:443)")
-	flag.StringVar(&flagWhitelist, "w", flagWhitelist, "-w whitelist-request (e.g. -w akamai.net:80)")
-	flag.IntVar(&config.Inject.MeekType, "mt", config.Inject.MeekType, "-mt meek type (0 and 1 for fastly)")
-	flag.IntVar(&config.PsiphonCore, "c", config.PsiphonCore, "-c core (e.g. -c 4) (1 for Pro Version)")
-	flag.StringVar(&config.Psiphon.Region, "r", config.Psiphon.Region, "-r region (e.g. -r sg)")
-	flag.IntVar(&config.Psiphon.Tunnel, "t", config.Psiphon.Tunnel, "-t tunnel (e.g. -t 4) (1 for Reconnect Version)")
-	flag.IntVar(&config.Psiphon.TunnelWorkers, "tw", config.Psiphon.TunnelWorkers, "-tw tunnel-workers (e.g. -tw 6) (8 for Pro Version)")
-	flag.IntVar(&config.Psiphon.KuotaDataLimit, "l", config.Psiphon.KuotaDataLimit, "-l limit (in MB) (e.g. -l 4) (0 for Pro Version (unlimited))")
-	flag.Parse()
-
-	if !flagPro {
-		config.Psiphon.Authorizations = make([]string, 0)
+	if len(p.Config.Authorizations) != 0 {
+		data = append(data, p.Config.Authorizations[0])
+		p.Config.Authorizations = append(p.Config.Authorizations[1:], p.Config.Authorizations[0])
 	}
 
-	if flagRefresh {
-		libpsiphon.RemoveData()
+	return data
+}
+
+func (p *Psiphon) CheckKuotaDataLimit(sent float64, received float64) bool {
+	if p.Config.KuotaDataLimit != 0 && int(p.KuotaData.Port[p.ListenPort]["all"]) >= (p.Config.KuotaDataLimit*1000000) &&
+		int(sent) == 0 && int(received) <= 64000 {
+		return false
 	}
 
-	if flagFrontend != "" || flagWhitelist != "" {
-		if flagFrontend == "" {
-			flagFrontend = "*"
-		}
-		if flagWhitelist == "" {
-			flagWhitelist = "*:*"
-		}
+	return true
+}
 
-		config.Inject.Rules = map[string][]string{
-			flagWhitelist: strings.Split(flagFrontend, ","),
-		}
+func (p *Psiphon) Start() {
+	PsiphonData := &Data{
+		MigrateDataStoreDirectory: ConfigPathPsiphon + "/data/" + strconv.Itoa(p.ListenPort),
+		UpstreamProxyURL:          "http://127.0.0.1:" + p.ProxyPort,
+		LocalSocksProxyPort:       p.ListenPort,
+		SponsorId:                 "00000000000000FF",
+		PropagationChannelId:      "00000000000000FF",
+		EmitBytesTransferred:      true,
+		EmitDiagnosticNotices:     true,
+		DisableLocalHTTPProxy:     true,
+		EgressRegion:              strings.ToUpper(p.Config.Region),
+		TunnelPoolSize:            p.Config.Tunnel,
+		ConnectionWorkerPoolSize:  p.Config.TunnelWorkers,
+		LimitTunnelProtocols:      p.Config.Protocols,
+		Authorizations:            p.GetAuthorizations(),
 	}
 
-	ProxyRotator := new(libproxyrotator.ProxyRotator)
-	ProxyRotator.Config = config.ProxyRotator
+	libutils.JsonWrite(PsiphonData, PsiphonData.MigrateDataStoreDirectory+"/config.json")
 
-	Inject := new(libinject.Inject)
-	Inject.Redsocks = Redsocks
-	Inject.Config = config.Inject
-
-	go ProxyRotator.Start()
-	go Inject.Start()
-
-	time.Sleep(200 * time.Millisecond)
-
-	liblog.LogInfo("Domain Fronting running on port "+Inject.Config.Port, "INFO", liblog.Colors["G1"])
-	liblog.LogInfo("Proxy Rotator running on port "+ProxyRotator.Config.Port, "INFO", liblog.Colors["G1"])
-
-	if _, err := os.Stat(libutils.RealPath(config.Psiphon.CoreName)); os.IsNotExist(err) {
-		liblog.LogInfo(
-			fmt.Sprintf(
-				"Exception:\n\n"+
-					"|	 File '%s' not exist!\n"+
-					"|	 Exiting...\n"+
-					"|\n",
-				config.Psiphon.CoreName,
-			),
-			"INFO", liblog.Colors["R1"],
+	PsiphonFileBoltdb := PsiphonData.MigrateDataStoreDirectory + "/ca.psiphon.PsiphonTunnel.tunnel-core/datastore/psiphon.boltdb"
+	if _, err := os.Stat(PsiphonFileBoltdb); os.IsNotExist(err) {
+		libutils.CopyFile(
+			libutils.RealPath("/storage/psiphon/database/psiphon.boltdb"), PsiphonFileBoltdb,
 		)
-		return
 	}
 
-	Redsocks.Config = libredsocks.DefaultConfig
-	Redsocks.Config.LogOutput = GetConfigPath("redsocks.log")
-	Redsocks.Config.ConfigOutput = GetConfigPath("redsocks.conf")
-	Redsocks.Start()
+	p.LogInfo("Connecting", liblog.Colors["G1"])
 
-	for i := 1; i <= config.PsiphonCore; i++ {
-		Psiphon := new(libpsiphon.Psiphon)
-		Psiphon.ProxyRotator = ProxyRotator
-		Psiphon.Config = config.Psiphon
-		Psiphon.ProxyPort = Inject.Config.Port
-		Psiphon.KuotaData = libpsiphon.DefaultKuotaData
-		Psiphon.ListenPort = libutils.Atoi(ProxyRotator.Config.Port) + i
-		Psiphon.Verbose = flagVerbose
+	for Loop {
+		p.KuotaData.Port[p.ListenPort] = make(map[string]float64)
+		p.KuotaData.Port[p.ListenPort]["all"] = 0
+		p.TunnelConnected = 0
 
-		go Psiphon.Start()
+		command := exec.Command(
+			libutils.RealPath(p.Config.CoreName), "-config", PsiphonData.MigrateDataStoreDirectory+"/config.json",
+		)
+		command.Dir = PsiphonData.MigrateDataStoreDirectory
+
+		stderr, err := command.StderrPipe()
+		if err != nil {
+			panic(err)
+		}
+
+		scanner := bufio.NewScanner(stderr)
+		go func() {
+			var text string
+			var line map[string]interface{}
+			for Loop && scanner.Scan() {
+				text = scanner.Text()
+				json.Unmarshal([]byte(text), &line)
+
+				noticeType := line["noticeType"]
+
+				if noticeType == "BytesTransferred" {
+					data := line["data"].(map[string]interface{})
+					diagnosticID := data["diagnosticID"].(string)
+					sent := data["sent"].(float64)
+					received := data["received"].(float64)
+
+					p.KuotaData.Port[p.ListenPort][diagnosticID] += sent + received
+					p.KuotaData.Port[p.ListenPort]["all"] += sent + received
+					p.KuotaData.All += sent + received
+
+					if p.CheckKuotaDataLimit(sent, received) == false {
+						break
+					}
+
+					liblog.LogReplace(
+						fmt.Sprintf(
+							"%v (%v) (%v) (%v)",
+							p.ListenPort,
+							diagnosticID,
+							libutils.BytesToSize(p.KuotaData.Port[p.ListenPort][diagnosticID]),
+							libutils.BytesToSize(p.KuotaData.All),
+						),
+						liblog.Colors["G1"],
+					)
+
+				} else if noticeType == "ActiveTunnel" {
+					p.ProxyRotator.AddProxy("0.0.0.0:" + strconv.Itoa(p.ListenPort))
+					p.TunnelConnected++
+					if p.Config.Tunnel > 1 {
+						diagnosticID := line["data"].(map[string]interface{})["diagnosticID"].(string)
+						p.LogInfo(fmt.Sprintf("Connected (%s)", diagnosticID), liblog.Colors["Y1"])
+					}
+					if p.TunnelConnected == p.Config.Tunnel {
+						p.LogInfo("Connected", liblog.Colors["Y1"])
+					}
+
+				} else if noticeType == "Alert" || noticeType == "Warning" {
+					message := line["data"].(map[string]interface{})["message"].(string)
+
+					if strings.HasPrefix(message, "Config migration:") {
+						continue
+					} else if strings.Contains(message, "meek round trip failed") {
+						if p.Config.Tunnel == 1 && p.Config.Tunnel == p.TunnelConnected && (message == "meek round trip failed: remote error: tls: bad record MAC" ||
+							message == "meek round trip failed: context deadline exceeded" ||
+							message == "meek round trip failed: EOF" ||
+							strings.Contains(message, "psiphon.CustomTLSDial")) {
+							p.LogVerbose(text, liblog.Colors["R1"])
+							break
+						}
+					} else if strings.Contains(message, "controller shutdown due to component failure") ||
+						strings.Contains(message, "psiphon.(*ServerContext).DoConnectedRequest") ||
+						strings.Contains(message, "psiphon.(*Tunnel).sendSshKeepAlive") ||
+						strings.Contains(message, "psiphon.(*Tunnel).Activate") ||
+						strings.Contains(message, "underlying conn is closed") ||
+						strings.Contains(message, "duplicate tunnel:") ||
+						strings.Contains(message, "tunnel failed:") {
+						p.LogVerbose(text, liblog.Colors["R1"])
+						break
+					} else if strings.Contains(message, "A connection attempt failed because the connected party did not properly respond after a period of time") {
+						p.LogVerbose(text, liblog.Colors["R1"])
+						break
+					}
+
+					p.LogVerbose(text, liblog.Colors["R1"])
+				}
+			}
+		}()
+
+		command.Run()
+
+		p.LogVerbose("Reconnecting", liblog.Colors["B1"])
+		libutils.Sleep(time.Second * 3)
 	}
 
-	InterruptHandler.Wait()
+	liblog.LogDelete(p.ListenPort, liblog.Colors["R1"])
 }
